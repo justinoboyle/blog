56a22731fec8797b08987c1f5bd03f60bea871e7
Justin <arraypro07@gmail.com>
2016-07-31 22:54:37 -0400

    stuff

--- index.js
+++ index.js
@@ -1,53 +1,67 @@
-var express = require('express');
-var fs = require('fs');
-var app = express();
-var staticDirBase = __dirname + '/public';
-app.set('port', (process.env.PORT || 5000));
+"use strict";
+const express = require('express'),
+    pg = require('pg');
 
-app.get('/static/*', function(request, response) {
-    var pathString = fixURL(request.url, { removeQuestionMark: true });
-    fs.lstat(pathString, function(err, stats) {
-        if (err) {
-            // send 404 not found
-            response.status(404);
-            response.send("That page could not be found. Please try again.");
-            return;
-        }
-        // the file exists.
-        if (stats.isDirectory()) {
-          response.status(404);
-          response.send("That page could not be found. Please try again.");
-          return;
-        }
-        response.sendFile(__dirname + "/" + pathString);
+const app = express();
+
+const dbURL = process.env.DATABASE_URL || require('./config.json').database_url;
+
+const staticDirBase = __dirname + '/public';
+
+const port = process.env.PORT || 3000;
+
+const websitePrefix = "http://localhost:3000"
+
+app.set('view engine', 'ejs');
+app.use('/static', express.static(staticDirBase));
+
+pg.defaults.ssl = true;
+
+GLOBAL.dbClient = new pg.Client(dbURL);
+
+dbClient.connect((err) => {
+    if (err)
+        return console.log(err);
+    dbClient.query("CREATE TABLE IF NOT EXISTS POSTS(id TEXT, title TEXT, content TEXT, categories TEXT, timestamp TEXT);");
+});
+
+app.listen(port, () => {
+    console.log("Listening on port " + port);
+});
+
+app.get('/post/:post', (req, res) => {
+    let post = req.params.post;
+    dbClient.query("SELECT id,title,content,categories,timestamp FROM POSTS WHERE id=$1", [post], (err, dbRes) => {
+        if (err)
+            return res.send("Oops, an error occured.");
+        if (dbRes.rows.length <= 0)
+            return res.send("oops, an error occured.");
+        res.render(__dirname + '/pages/post', { post: dbRes.rows[0] });
     });
 });
 
-app.get("/*", function(request, response) {
-  response.status(404);
-  response.send("That page could not be found. Please try again.");
-  return;
+app.get('/:page*?', (req, res) => {
+    let limit = 10;
+    let page = parseInt(req.params.page) || 1;
+    let offset = (page - 1) * limit;
+    console.log(offset);
+    dbClient.query("SELECT id,title,content,categories,timestamp FROM POSTS ORDER BY TIMESTAMP DESC OFFSET($1) LIMIT($2)", [offset, limit], (err, dbRes) => {
+        if (err)
+            return res.send("Oops, an error occured.");
+        res.render(__dirname + '/pages/index', { posts: dbRes.rows, nextPage: page + 1 });
+    });
 });
 
-function getRawPost(title) {
-  // TODO Get raw post data
-}
-
-function fixURL(url, opt) {
-    var options = opt || {};
-    if(typeof(options.removeQuestionMark) != "undefined" && options.removeQuestionMark)
-      while (url.includes("?")) url = url.substring(0, url.indexOf('?'));
-    while (url.startsWith("/")) url = url.substring(1);
-    while (url.includes("../")) url = url.replace("..", "");
-    while (endsWith(url, "/")) url = url.slice(0, -1);
-    while (url.includes("//")) url = url.replace("//", "/");
-    return url;
-}
-
-function endsWith(str, suffix) {
-    return str.indexOf(suffix, str.length - suffix.length) !== -1;
-}
-
-app.listen(app.get('port'), function() {
-    console.log("Node app is running at localhost:" + app.get('port'));
-});
+app.get('/sitemap.xml', (req, res) => {
+    dbClient.query("SELECT id FROM POSTS ORDER BY TIMESTAMP DESC", [offset, limit], (err, dbRes) => {
+        let build = [];
+        for(let row of dbRes.rows)
+            build.push('  <url><loc>' + websitePrefix + '/post/' + row.id + '</url></loc>');
+        return res.send([
+            "<?xml version='1.0' encoding='UTF-8'?>",
+            '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
+            build.join('\n'),
+            '</urlset>'
+        ]).join('\n');
+    });
+})
